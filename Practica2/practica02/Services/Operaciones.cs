3a107e50c130bd3c6b9d1f9721f3fe6bf8fa95af
AlanAHD <alanarmando997@gmail.com>
2023-02-28 11:16:39 -0600

    Practica 2

--- Practica2/practica02/Services/Operaciones.cs
+++ Practica2/practica02/Services/Operaciones.cs
@@ -1,17 +1,34 @@
 using System;
 using WSDL.mensajes;
 
-namespace WSDL.operaciones
-{
-    public class Operaciones : Mensajes
-    {
+namespace WSDL.operaciones{
+    public class Operaciones : Mensajes{
+        private string[] mensajes = new string[2];
+ 
         public string Saludar(string nombre){
-            string msj= "Hola" +nombre;
-            return msj;
-        }
+            Boolean estaLleno = true;
+            for(int x = 0; x < mensajes.Length; x++){
+                if(string.IsNullOrEmpty(mensajes[x])){
+                    mensajes[x]=nombre;
+                    estaLleno = false;
+                    break;
+                }
+            }
 
+            if (estaLleno == true){
+                return "Memoria llena..";
+            }
+            return "Hola "+nombre;
+        }
         public string Mostrar(int id){
-            return "x";
+            if(id >= mensajes.Length || id < 0){
+                return "No existe este id";
+            } 
+            
+            if(!string.IsNullOrEmpty(mensajes[id])){
+                return "Hola y bienvenido " + mensajes[id];
+            }
+            return "No se encontro el mensaje con el id";
         }
     }
 }
